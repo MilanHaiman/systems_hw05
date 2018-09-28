e3989aa2e045f4d01c958455196217cb97d12f6a
MilanHaiman <mhaiman@stuy.edu>
2018-09-28 10:57:02 -0400

    mystrncpy

--- mh.c
+++ mh.c
@@ -10,10 +10,47 @@ int mystrlen(char *s) {
 	return i;
 }
 
-int main(){
-	char *s1 = "hello";
-	printf("%d\n", mystrlen(s1));
-	s1 = "";
-	printf("%d\n", mystrlen(s1));
-	
+int mystrcmp( char *s1, char *s2) {
+	int i = 0;
+	while (s1[i]==s2[i]) {
+		if (!s1[i]) {
+			return 0;
+		}
+		i++;
+	}
+	return s1[i]-s2[i];
 }
+
+char * mystrncpy( char *dest, char *source, int n) {
+	char * output = & dest[mystrlen(dest)];
+	int i = 0;
+	while (i < n && source[i]) {
+		dest[i] = source[i];
+		i++;
+	}
+	while (i<n) {
+		dest[i] = 0;
+	}
+	if (!output[0]) {
+		output = & dest[n];
+	}
+}
+
+
+int main(){
+	char *s0 = "hello";
+	printf("length of \"hello\": %d\n", mystrlen(s0));
+	s0 = "";
+	printf("length of empty string: %d\n", mystrlen(s0));
+
+	char s1[] = "hello";
+	char s2[] = "hill";
+	printf("Should be negative: \t%d\n",strcmp(s1,s2)); 
+	char s3[] = "abce";
+	char s4[] = "abcd";
+	printf("Should be positive: \t%d\n",strcmp(s3,s4));
+
+	
+
+}
+
