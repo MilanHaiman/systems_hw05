e3c03a8b723b37e9c213497ba2a42cea3368a6e1
Zer0-M <msj2340@gmail.com>
2018-09-29 03:17:24 -0400

    added a working version of strcat, which can be improved

--- mj.c
+++ mj.c
@@ -11,10 +11,26 @@ char * mystrchr( char *s, char c){
   return NULL;
 
 }
+char * mystrcat( char *dest, char *source){
+  char *start=dest;
+  while(*dest){
+    dest++;
+  }
+  while(*source){
+    *dest=*source;
+    source++;
+    dest++;
+  }
+  return start;
+
+}
 int main(){
   char *s1="order";
+  char s2[10]="dis";
   char c='r';
   char *cp=mystrchr(s1,c);
+  char *concat=mystrcat(s2,s1);
   printf("Address of r in order: %p\n",cp);
   printf("String from first r: %s\n",cp);
+  printf("Concatenation of dis and order: %s\n",concat);
 }
