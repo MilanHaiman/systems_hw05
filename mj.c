905237179800ba4572b7098ca571e06d34f0e946
Zer0-M <mjamil90@stuy.edu>
2018-09-29 14:12:38 -0400

    created a working version of strstr

--- mj.c
+++ mj.c
@@ -12,25 +12,39 @@ char * mystrchr( char *s, char c){
 
 }
 char * mystrcat( char *dest, char *source){
-  char *start=dest;
-  while(*dest){
-    dest++;
-  }
+  char *end=dest+strlen(dest);
   while(*source){
-    *dest=*source;
+    *end=*source;
     source++;
-    dest++;
+    end++;
   }
-  return start;
-
+  return dest;
+}
+char * mystrstr( char *s1, char * s2 ){
+  char *first=mystrchr(s1,*s2);
+  for(int i=0;i<strlen(s2);i++){
+    if(first[i]!=s2[i]){
+        if(first==NULL){
+         return NULL;
+        }
+        else{
+          first=mystrchr(first+1,*s2);
+          i=0;
+        }
+    }
+  }
+  return first;
 }
 int main(){
   char *s1="order";
   char s2[10]="dis";
+  char *s3="er";
+  char *s4="Peter Piper picked a peck of pickled peppers.";
   char c='r';
   char *cp=mystrchr(s1,c);
   char *concat=mystrcat(s2,s1);
   printf("Address of r in order: %p\n",cp);
   printf("String from first r: %s\n",cp);
   printf("Concatenation of dis and order: %s\n",concat);
+  printf("%s\n",mystrstr(s4,s3));
 }
